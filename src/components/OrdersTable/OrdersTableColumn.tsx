28c8b093ccf5112bdd0d2b94e9a25988a3cc3326
nikhea <imonikheaugbodaga@gmail.com>
2023-05-25 22:44:10 +0100

    fix: order table

--- src/components/OrdersTable/OrdersTableColumn.tsx
+++ src/components/OrdersTable/OrdersTableColumn.tsx
@@ -58,62 +58,68 @@ export const Ordercolumns = [
     headerAlign: "center",
   },
   {
-    field: "action",
+    field: "status",
     headerName: "status",
     flex: 1,
-    // width: "70%",
-    headerAlign: "center",
     cellClassName: "name-column--cell",
-    renderCell: (params: any) => {
-      const status = params.row.status;
-      const [selectedStatus, setSelectedStatus] = useState(status);
-
-      const handleStatusChange = (event: any) => {
-        const newStatus = event.target.value;
-        // TODO: Update the status in your data source (e.g., OrdersTableData) to newStatus
-        setSelectedStatus(newStatus);
-      };
-
-      let statusStyle;
-      switch (status) {
-        case "pending":
-          statusStyle = "text-yellow-500 bg-yellow-100";
-          break;
-        case "processing":
-          statusStyle = "text-blue-500 bg-blue-100";
-          break;
-        case "shipped":
-          statusStyle = "text-green-500 bg-green-100";
-          break;
-        case "delivered":
-          statusStyle = "text-gray-500 bg-gray-100";
-          break;
-        default:
-          statusStyle = "text-gray-500 bg-gray-100";
-      }
-
-      return (
-        <div className="flex items-center w-[100%] !text-xs border-none">
-          <Select
-            value={selectedStatus}
-            onChange={handleStatusChange}
-            className={`${statusStyle} rounded-[25px]`}
-            IconComponent={FaAngleDown}
-            style={{
-              width: "100%",
-              borderRadius: "25px",
-              outline: "none",
-              border: "none",
-            }}
-          >
-            {statusState.map((state) => (
-              <MenuItem key={state} value={state}>
-                {state}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
-      );
-    },
+    headerAlign: "center",
   },
+  // {
+  //   field: "action",
+  //   headerName: "status",
+  //   flex: 1,
+  //   // width: "70%",
+  //   headerAlign: "center",
+  //   cellClassName: "name-column--cell",
+  //   renderCell: (params: any) => {
+  //     const status = params.row.status;
+  //     const [selectedStatus, setSelectedStatus] = useState(status);
+
+  //     const handleStatusChange = (event: any) => {
+  //       const newStatus = event.target.value;
+  //       setSelectedStatus(newStatus);
+  //     };
+
+  //     let statusStyle;
+  //     switch (status) {
+  //       case "pending":
+  //         statusStyle = "text-yellow-500 bg-yellow-100";
+  //         break;
+  //       case "processing":
+  //         statusStyle = "text-blue-500 bg-blue-100";
+  //         break;
+  //       case "shipped":
+  //         statusStyle = "text-green-500 bg-green-100";
+  //         break;
+  //       case "delivered":
+  //         statusStyle = "text-gray-500 bg-gray-100";
+  //         break;
+  //       default:
+  //         statusStyle = "text-gray-500 bg-gray-100";
+  //     }
+
+  //     return (
+  //       <div className="flex items-center w-[100%] !text-xs border-none">
+  //         <Select
+  //           value={selectedStatus}
+  //           onChange={handleStatusChange}
+  //           className={`${statusStyle} rounded-[25px]`}
+  //           IconComponent={FaAngleDown}
+  //           style={{
+  //             width: "100%",
+  //             borderRadius: "25px",
+  //             outline: "none",
+  //             border: "none",
+  //           }}
+  //         >
+  //           {statusState.map((state) => (
+  //             <MenuItem key={state} value={state}>
+  //               {state}
+  //             </MenuItem>
+  //           ))}
+  //         </Select>
+  //       </div>
+  //     );
+  //   },
+  // },
 ];
