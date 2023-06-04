18eb13a68213a6bfc54f7faeaddb814b1a6cee4d
nikhea <imonikheaugbodaga@gmail.com>
2023-06-04 20:25:52 +0100

    issues: order table

--- page/Orders/page.tsx
+++ page/Orders/page.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import OrdersTable from "../../src/components/OrdersTable/OrdersTable";
 
 const Orders = () => {
-  return (
-    <div>
-      <OrdersTable />
-    </div>
-  );
+  return <div>{/* <OrdersTable /> */}</div>;
 };
 
 export default Orders;
