be90bb259c79833d74e31901e1369529e2761280
nikhea <imonikheaugbodaga@gmail.com>
2023-10-08 01:58:30 +0100

    added render

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,10 +2,9 @@ import axios from "axios";
 import { storage } from "../auth/utils";
 
 let main = import.meta.env.VITE_HEKTO_PRODUCT;
-let app = " https://hekto-v4oa.onrender.com/api/";
 let test = import.meta.env.VITE_HEKTO_DEVELOPMENT;
 const instance = axios.create({
-  baseURL: app,
+  baseURL: main,
   headers: {
     Authorization: `Bearer ${storage.getToken()}`,
   },
