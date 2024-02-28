5b06f43f1a7fb3ec198fc6e9537a69042540bd66
nikhea <imonikheaugbodaga@gmail.com>
2024-02-28 02:34:15 +0100

    wip: customer table

--- page/users/page.tsx
+++ page/users/page.tsx
@@ -1,9 +1,38 @@
+import { useFetchAllUser } from "../../src/Hooks/useUser/useFetchAllUser";
+import PageLoading from "../../src/components/Loading/PageLoading";
 import Table from "../../src/components/UsersComponents/Table";
+import { formatDate } from "../../src/utils/getFullYear";
 
 const Users = () => {
+  const { users, isLoading } = useFetchAllUser();
+
+  if (isLoading) {
+    return <PageLoading />;
+  }
+  const data: any = [];
+
+  if (users) {
+    users.forEach((item: any) => {
+      const users = {
+        id: item._id,
+        image:
+          item.profile?.profileImage?.thumbnail_url ||
+          "https://res.cloudinary.com/djkqaqoj3/image/upload/v1686020514/cover-productImages-undefined-1686020113705/qggstfyhcspzpklr4bhl.jpg",
+        first_name: item.firstname,
+        last_name: item.lastname,
+        email: item.email,
+        date: formatDate(item.createdAt),
+        country: item.shipping?.address.country || "nigeria",
+        phone: item.shipping?.address.phone || "No phone number",
+      };
+
+      data.push(users);
+    });
+  }
+
   return (
     <div>
-      <Table />
+      <Table users={data} />
     </div>
   );
 };
