181c6cf54cf3a13135795e751428bd7ebf1e6d1c
nikhea <imonikheaugbodaga@gmail.com>
2024-02-28 03:45:37 +0100

    wip: order stats

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,7 @@ import { storage } from "../auth/utils";
 let main = import.meta.env.VITE_HEKTO_PRODUCT;
 let test = import.meta.env.VITE_HEKTO_DEVELOPMENT;
 const instance = axios.create({
-  baseURL: test,
+  baseURL: main,
   headers: {
     Authorization: `Bearer ${storage.getToken()}`,
   },
